3a70f6565ae1e334f7cae5cbff5607994a359672
tomoki23 <tmdesign.0223@gmail.com>
2023-02-08 20:03:16 +0900

    StockController indexアクション追加

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class StockController extends Controller
 {
-    //
+    public function index()
+    {
+        $tools = [
+            ['name' => '刷毛', 'quantity' => 20],
+            ['name' => 'ローラー', 'quantity' => 40],
+            ['name' => 'シンナー', 'quantity' => 3],
+        ];
+
+        return view('index', compact('tools'));
+    }
 }
