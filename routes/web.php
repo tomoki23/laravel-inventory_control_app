15c5ce48aac16999cf225182991c30b511ff9212
tomoki23 <tmdesign.0223@gmail.com>
2023-02-08 20:02:40 +0900

    ルート修正

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/stocks', function () {
-    return view('index');
-});
+Route::get('/stocks', [StockController::class, 'index'])->name('index');
