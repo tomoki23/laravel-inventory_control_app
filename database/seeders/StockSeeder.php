85bd6589f3f6071ac230babce3fa28f87bb3ce9a
tomoki23 <tmdesign.0223@gmail.com>
2023-02-11 14:01:51 +0900

    シーダー ダミーデータの作成と呼び出し

--- database/seeders/StockSeeder.php
+++ database/seeders/StockSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Stock;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,25 @@ class StockSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Stock::insert([
+            'tool_name' => '刷毛',
+            'quantity' => 10,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        Stock::insert([
+            'tool_name' => 'ローラー',
+            'quantity' => 20,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        Stock::insert([
+            'tool_name' => 'シンナー',
+            'quantity' => 3,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
