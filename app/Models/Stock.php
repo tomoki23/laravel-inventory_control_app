4f2baa0a4d56a2a9749d6c55e91076888571e51d
tomoki23 <tmdesign.0223@gmail.com>
2023-02-13 13:30:11 +0900

    登録処理作成

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Stock extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'tool_name',
+        'quantity',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function RegisterStock($stock)
+    {
+        $this->create([
+            'tool_name' => $stock['tool_name'],
+            'quantity' => $stock['quantity']
+        ]);
+    }
 }
