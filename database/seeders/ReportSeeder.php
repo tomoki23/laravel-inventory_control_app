b9b9292c2c7fa82a0bf14092ed8cbc0f9826fe77
tomoki23 <tmdesign.0223@gmail.com>
2023-02-15 15:20:34 +0900

    ダミーデータ登録内容

--- database/seeders/ReportSeeder.php
+++ database/seeders/ReportSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Report;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,28 @@ class ReportSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Report::insert([
+            'user_name' => 'テストユーザー1',
+            'member' => 'メンバー1',
+            'content' => '今日の業務内容は〇〇です',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Report::insert([
+            'user_name' => 'テストユーザー2',
+            'member' => 'メンバー2',
+            'content' => '今日の業務内容は〇〇です',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Report::insert([
+            'user_name' => 'テストユーザー3',
+            'member' => 'メンバー3',
+            'content' => '今日の業務内容は〇〇です',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
