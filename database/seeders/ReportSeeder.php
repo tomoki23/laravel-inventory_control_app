2df30278a182e6a1c8a72835a53480b4b2fab758
tomoki23 <tmdesign.0223@gmail.com>
2023-02-15 15:54:46 +0900

    ダミーデータsite_name追加

--- database/seeders/ReportSeeder.php
+++ database/seeders/ReportSeeder.php
@@ -17,6 +17,7 @@ class ReportSeeder extends Seeder
     {
         Report::insert([
             'user_name' => 'テストユーザー1',
+            'site_name' => '鈴木邸',
             'member' => 'メンバー1',
             'content' => '今日の業務内容は〇〇です',
             'created_at' => now(),
@@ -25,6 +26,7 @@ class ReportSeeder extends Seeder
 
         Report::insert([
             'user_name' => 'テストユーザー2',
+            'site_name' => '佐藤邸',
             'member' => 'メンバー2',
             'content' => '今日の業務内容は〇〇です',
             'created_at' => now(),
@@ -33,6 +35,7 @@ class ReportSeeder extends Seeder
 
         Report::insert([
             'user_name' => 'テストユーザー3',
+            'site_name' => '高橋邸',
             'member' => 'メンバー3',
             'content' => '今日の業務内容は〇〇です',
             'created_at' => now(),
