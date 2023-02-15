119f891ac83ccddc11a74de344f40efcb4b1c3d8
tomoki23 <tmdesign.0223@gmail.com>
2023-02-15 15:32:46 +0900

    indexアクション 簡単な日報一覧画面作成

---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>日報一覧画面</title>
+</head>
+
+<body>
+  {{-- ヘッダーここから --}}
+  <header>
+    <h1>日報一覧画面</h1>
+    <nav>
+      <ul>
+        <li><a href="{{ route('stocks.index') }}">管理画面</a></li>
+        <li><a href="#">日報登録画面</a></li>
+        <li><a href="{{ route('reports.index') }}">日報一覧画面</a></li>
+      </ul>
+    </nav>
+  </header>
+  {{-- ヘッダーここまで --}}
+
+  {{-- メインここから --}}
+  <main>
+
+</body>
+
+</html>
