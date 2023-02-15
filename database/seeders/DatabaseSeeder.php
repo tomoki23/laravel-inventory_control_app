a393a5c1f1cc278d8047ba5a5b15e9adb304f903
tomoki23 <tmdesign.0223@gmail.com>
2023-02-15 15:20:43 +0900

    reportseeder呼び出し

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            StockSeeder::class
+            StockSeeder::class,
+            ReportSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
