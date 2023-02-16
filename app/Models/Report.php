b87100b512f73104a8ea8f66e70e92b943e7ada6
tomoki23 <tmdesign.0223@gmail.com>
2023-02-16 20:38:25 +0900

    登録処理

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -4,8 +4,47 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Console\Input\Input;
 
 class Report extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_name',
+        'site_name',
+        'member',
+        'content',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function registerReport($report)
+    {
+
+        // dd($report);
+        $user_name = $report->input('user_name');
+        $site_name = $report->input('site_name');
+        $members = $report->input('member');
+        $content = $report->input('content');
+
+        foreach ($members as $key => $val) {
+            if ($val === '1') {
+                $member_list[] = 'ユーザー1';
+            } else if ($val === '2') {
+                $member_list[] = 'ユーザー2';
+            } else if ($val === '3') {
+                $member_list[] = 'ユーザー3';
+            }
+        }
+
+        $members = implode(',', $member_list);
+
+        $this->create([
+            'user_name' => $user_name,
+            'site_name' => $site_name,
+            'member' => $members,
+            'content' => $content
+        ]);
+    }
 }
