b94449ddabfbcf0ba8bef96bcb7b20dd848bfe32
tomoki23 <tmdesign.0223@gmail.com>
2023-02-17 21:13:56 +0900

    updateの引数修正

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -76,7 +76,7 @@ class ReportController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ReportPostRequest $request, $id)
     {
         $report = Report::find($id);
 
