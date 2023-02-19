cedf7e05ef3f67aa643f34c438701e2db411068e
tomoki23 <tmdesign.0223@gmail.com>
2023-02-19 21:44:42 +0900

    registerStock()削除

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -15,14 +15,4 @@ class Stock extends Model
         'created_at',
         'updated_at'
     ];
-
-    public function RegisterStock($stock)
-    {
-        $tool_name = $stock->input('tool_name');
-        $quantity = $stock->input('quantity');
-        $this->create([
-            'tool_name' => $tool_name,
-            'quantity' => $quantity
-        ]);
-    }
 }
