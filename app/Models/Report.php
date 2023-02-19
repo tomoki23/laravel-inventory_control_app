50e07bf209af6caa2db1c541042a687c3c5a8114
tomoki23 <tmdesign.0223@gmail.com>
2023-02-19 21:32:58 +0900

    registerReport()削除

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -18,34 +18,4 @@ class Report extends Model
         'created_at',
         'updated_at'
     ];
-
-    public function registerReport($report)
-    {
-
-        //リクエスト情報を取得
-        $user_name = $report->input('user_name');
-        $site_name = $report->input('site_name');
-        $members = $report->input('member');
-        $content = $report->input('content');
-
-        // メンバーの初期化
-        $member_list = [];
-
-        // メンバーリストに代入
-        foreach ($members as $key => $val) {
-            $member_list[] = $val;
-        }
-
-        //データベースに保存するために配列の要素を連結
-        $members = implode(',', $member_list);
-
-
-        //登録
-        $this->create([
-            'user_name' => $user_name,
-            'site_name' => $site_name,
-            'member' => $members,
-            'content' => $content
-        ]);
-    }
 }
