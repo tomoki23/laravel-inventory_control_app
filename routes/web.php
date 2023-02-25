123ce4f57a3132cfbe49695de27d24fa390f2061
tomoki23 <tmdesign.0223@gmail.com>
2023-02-25 13:53:12 +0900

    ルート追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::resource('stocks', StockController::class);
+Route::resource('reports', ReportController::class);
