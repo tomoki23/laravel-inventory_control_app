dff8bb019ab47e41d3733e755d19c0d7d7e67ee4
tomoki23 <tmdesign.0223@gmail.com>
2023-02-25 21:51:59 +0900

    仮ui作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,80 +1,67 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>在庫管理画面</title>
-</head>
-<body>
-  {{-- ヘッダーここから --}}
-  <header>
-    <h1>在庫管理画面</h1>
-    <nav>
-      <ul>
-        <li><a href="{{ route('stocks.index') }}">管理画面</a></li>
-        <li><a href="{{ route('reports.create') }}">日報登録画面</a></li>
-        <li><a href="{{ route('reports.index') }}">日報一覧画面</a></li>
-      </ul>
-    </nav>
-  </header>
-  {{-- ヘッダーここまで --}}
+<x-app-layout>
+  <div class="w-3/4 mt-8 mx-auto">
+    {{-- 追加フォーム --}}
+    <form action="{{ route('stocks.store') }}" method="POST">
+      @csrf
+      <input type="text" name="tool_name">
+      <input type="number" name="quantity">
+      <x-primary-button type="submit">追加</x-primary-button>
+    </form>
 
-  {{-- メインここから --}}
-  <main>
-    <div>
-      {{-- 追加フォーム --}}
-      <form action="{{ route('stocks.store') }}" method="POST">
-        @csrf
-        <input type="text" name="tool_name">
-        <input type="number" name="quantity">
-        <button type="submit">追加</button>
-      </form>
+    {{-- エラー表示 --}}
+    {{-- エラーがあるか判定 --}}
+    @if ($errors->any())
+        <ul>
+          @foreach ($errors->all() as $message)
+              <li>{{ $message }}</li>
+          @endforeach
+        </ul>
+    @endif
 
-      {{-- エラー表示 --}}
-      {{-- エラーがあるか判定 --}}
-      @if ($errors->any())
-          <ul>
-            @foreach ($errors->all() as $message)
-                <li>{{ $message }}</li>
-            @endforeach
-          </ul>
-      @endif
-
-      {{-- 備品表示テーブル --}}
-      <table border="1">
-        <tr>
-          <th>備品名</th>
-          <th>備品の数</th>
-          <th>削除</th>
-        </tr>
-        @foreach ($tools as $tool)
+    <div class="relative overflow-x-auto mt-8">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <td>{{ $tool['tool_name'] }}</td>
-            <td>
-              {{-- カウントアップ用のフォーム --}}
-              <form action="{{ route('stocks.update', $tool->id) }}" method="POST">
-                @csrf
-                @method('PUT')
-                <button type="submit" name="plus">+</button>
-                <span>{{ $tool['quantity'] }}個</span>
-                <input type="hidden" name="id" value="{{ $tool['id'] }}">
-                <button type="submit" name="minus">-</button>
-              </form>
-            </td>
-            <td>
-              {{-- 削除用のフォーム --}}
-              <form action="{{ route('stocks.destroy', $tool->id)}}" method="POST">
-                @csrf
-                @method('delete')
-                <button type="submit" name="delete">削除</button>
-              </form>
-            </td>
+            <th scope="col" class="px-6 py-3">
+              備品名
+            </th>
+            <th scope="col" class="px-6 py-3">
+              備品の数
+            </th>
+            <th scope="col" class="px-6 py-3">
+              ボタン
+            </th>
           </tr>
+        </thead>
+        @foreach ($tools as $tool)
+          <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $tool['tool_name'] }}
+              </td>
+              <td class="px-6 py-4">
+                {{-- カウントアップ用のフォーム --}}
+                <form action="{{ route('stocks.update', $tool->id) }}" method="POST">
+                  @csrf
+                  @method('PUT')
+                  <x-primary-button type="submit" name="plus">+</x-primary-button>
+                  <span>{{ $tool['quantity'] }}個</span>
+                  <input type="hidden" name="id" value="{{ $tool['id'] }}">
+                  <x-primary-button type="submit" name="minus">-</x-primary-button>
+                </form>
+              </td>
+              <td class="px-6 py-4">
+                {{-- 削除用のフォーム --}}
+                <form action="{{ route('stocks.destroy', $tool->id)}}" method="POST">
+                  @csrf
+                  @method('delete')
+                  <x-danger-button type="submit" name="delete">削除</x-danger-button>
+                </form>
+              </td>
+            </tr>
+          </tbody>
         @endforeach
       </table>
     </div>
-  </main>
-  {{-- メインここまで --}}
-</body>
-</html>
+  </div>
+</x-app-layout>
