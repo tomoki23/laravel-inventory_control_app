1d896f1ee7ad4ab414f0ea1167eefc331e47fa97
tomoki23 <tmdesign.0223@gmail.com>
2023-03-08 20:42:11 +0900

    リレーションの設定

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -5,17 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Symfony\Component\Console\Input\Input;
+use App\Models\User;
 
 class Report extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'user_name',
+        'user_id',
         'site_name',
-        'member',
+        'member_id',
         'content',
         'created_at',
         'updated_at'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
