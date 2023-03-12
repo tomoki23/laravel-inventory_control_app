b64ece049d834753be91dbb5cc3515b51e4d447c
tomoki23 <tmdesign.0223@gmail.com>
2023-03-12 22:02:56 +0900

    UpdateReportRequest作成

---
+++ app/Http/Requests/UpdateReportRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateReportRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|min:1|max:50',
+            'site_name' => 'required|min:1|max:255',
+            'content' => 'required|min:1|max:1000'
+        ];
+    }
+}
